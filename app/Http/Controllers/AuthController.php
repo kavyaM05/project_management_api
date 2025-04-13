1e6a260b6f3666708a341c990b13c63e1c311451
Kavya <kavyamnair482@gmail.com>
2025-04-13 18:06:04 +0530

    prjct management api- login,registration

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -47,7 +47,6 @@ class AuthController extends Controller
     public function login(Request $request)
     {
         $credentials = $request->only('email', 'password');
-
         if (!$token = JWTAuth::attempt($credentials)) {
             return response()->json(['error' => 'Invalid credentials'], 401);
         }
