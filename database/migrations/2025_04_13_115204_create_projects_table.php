48b2aaacb40d4661973ba767f62620fc1a3853e1
Kavya <kavyamnair482@gmail.com>
2025-04-16 01:12:25 +0530

    prjct management api- login,registration

--- database/migrations/2025_04_13_115204_create_projects_table.php
+++ database/migrations/2025_04_13_115204_create_projects_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -14,8 +13,8 @@ return new class extends Migration
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
             $table->integer('user_id');
-            $table->integer('title');
-            $table->integer('description');
+            $table->char('title', 50);
+            $table->char('description', 100);
             $table->timestamps();
         });
     }
