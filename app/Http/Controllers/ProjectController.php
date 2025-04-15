ff7713b171b13172dbc8570b830c94cfdd7ce2bd
Kavya <kavyamnair482@gmail.com>
2025-04-16 01:12:37 +0530

    prjct management api- login,registration

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -3,14 +3,20 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Project;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ProjectController extends Controller
 {
+    /**
+     * @return JsonResponse
+     */
     public function index()
     {
-        return auth()->user();
+        $projects = Project::where('user_id', Auth::id())->get();
+        return response()->json($projects);
     }
 
     /**
@@ -19,11 +25,51 @@ class ProjectController extends Controller
      */
     public function store(Request $request)
     {
-        $data = $request->validate(['title' => 'required', 'description' => 'required']);
-        $project = auth()->user()->projects()->create($data);
-        return response()->json($project);
-    }
-    public function update(Request $request){
+        $request->validate([
+            'title' => 'required',
+            'description' => 'nullable',
+        ]);
 
+        $project = Project::create([
+            'title' => $request->input('title'),
+            'description' => $request->input('description'),
+            'user_id' => Auth::id(),
+        ]);
+
+        return response()->json(['message' => 'Project created', 'project' => $project], 201);
+    }
+
+    /**
+     * @param Request $request
+     * @param $id
+     * @return JsonResponse
+     */
+    public function update(Request $request, $id)
+    {
+        $project = Project::where('id', $id)->where('user_id', Auth::id())->firstOrFail();
+
+        $request->validate([
+            'title' => 'required',
+            'description' => 'nullable',
+        ]);
+
+        $project->update([
+            'title' => $request->title,
+            'description' => $request->description,
+        ]);
+
+        return response()->json(['message' => 'Project updated', 'project' => $project]);
+    }
+
+    /**
+     * @param $id
+     * @return JsonResponse
+     */
+    public function destroy($id)
+    {
+        $project = Project::where('id', $id)->where('user_id', Auth::id())->firstOrFail();
+        $project->delete();
+
+        return response()->json(['message' => 'Project deleted']);
     }
 }
